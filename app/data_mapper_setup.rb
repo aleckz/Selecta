ac0efcd8175f6a75c0941d168298ed2dc62556ce
Alex <aleksandernasr@gmail.com>
2015-07-16 22:35:34 +0100

    databasemapper HEROKU migrate

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -12,4 +12,4 @@ DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/selecta_
   require './app/models/user'
 
 DataMapper.finalize
-DataMapper.auto_upgrade!
+DataMapper.auto_migrate!
